d0ac908e66aa226806d1adf71f47cf9bdb96471a
Phạm Tấn Thanh <32680850+thanhsr@users.noreply.github.com>
2018-04-16 14:22:34 +0700

    Create she.js

---
+++ she.js
@@ -0,0 +1,43 @@
+<script type='application/ld+json'>
+{
+"@context":"http://schema.org",
+"@type":"Corporation",
+"name": "Danh sách bác sĩ đông y giỏi tại TPHCM",
+ "description":"Bác sĩ đông y giỏi chuyên chữa bệnh bằng phương pháp y học cổ truyền vừa an toàn vừa hiệu quả tại Sài Gòn - TPHCM.",
+"email":"yhoccotruyensg12@gmail.com",
+  "address": {
+    "@type": "PostalAddress",
+    "addressLocality": "Hồ Chí Minh",
+    "addressRegion": "Vietnam",
+    "postalCode": "700000",
+    "streetAddress": "1061B Cách mạng tháng Tám, Phường 7, Quận Tân Bình, Phường 7, Hồ Chí Minh"
+  },
+"telephone":"0961036748",
+"url":"http://bacsidongygioi.com/",
+"sameAs":[
+	"https://www.facebook.com/yhoccotruyensaigon/",
+	"https://www.instagram.com/bacsidongy",
+	"https://myspace.com/bacsidongy",
+	"https://plus.google.com/+B%C3%A1cs%C4%A9%C4%91%C3%B4ngygi%E1%BB%8Fi",
+	"https://www.youtube.com/channel/UCFNGZqh_-ioNbjZ4wTHEhlg",
+	"https://www.pinterest.com/bacsidongygioi/",
+	"https://twitter.com/bacsidongy",
+]
+}
+</script>
+<script type='application/ld+json'>
+{
+"@context":"http://schema.org/",
+"@type":"GeoCoordinates",
+"latitude":"10.791072",
+"longitude":"106.655676",
+"postalcode":"700000",
+ "address": {
+    "@type": "PostalAddress",
+    "addressLocality": "Hồ Chí Minh",
+    "addressRegion": "Vietnam",
+    "postalCode": "700000",
+    "streetAddress": "1061B Cách mạng tháng Tám, Phường 7, Quận Tân Bình, Phường 7, Hồ Chí Minh"
+  }
+}
+</script>
